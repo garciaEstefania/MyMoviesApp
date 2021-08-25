96d8e2f6d2954a17fd371dce421172a5e319e189
garciaEstefania <garcia_estefannia@hotmail.com>
2021-08-25 07:36:02 -0600

    initializing home screen

---
+++ app/screens/homeScreen.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { ImageBackground, Image, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import styles from "../assets/styles/style";
+
+export default class HomeScreen extends Component {
+    render() {
+        return (
+            <Text>Home Screen</Text>
+        )
+    }
+}
